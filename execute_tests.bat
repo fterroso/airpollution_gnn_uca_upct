1c253fd0d472399a1ee707cb64d27f3698a7e25a
Fernando Terroso-Saenz <f.terroso@yahoo.com>
2024-05-27 15:35:05 +0200

    Bash script to run GNN tests in WinOS

---
+++ execute_tests.bat
@@ -0,0 +1,19 @@
+python 04_gnn_prediction.py 1 6 bilbao True True
+python 04_gnn_prediction.py 1 12 bilbao True True
+python 04_gnn_prediction.py 1 24 bilbao True True
+python 04_gnn_prediction.py 1 48 bilbao True True
+
+python 04_gnn_prediction.py 1 6 bilbao False True
+python 04_gnn_prediction.py 1 12 bilbao False True
+python 04_gnn_prediction.py 1 24 bilbao False True
+python 04_gnn_prediction.py 1 48 bilbao False True
+
+python 04_gnn_prediction.py 2 6 bilbao True True
+python 04_gnn_prediction.py 2 12 bilbao True True
+python 04_gnn_prediction.py 2 24 bilbao True True
+python 04_gnn_prediction.py 2 48 bilbao True True
+
+python 04_gnn_prediction.py 2 6 bilbao False True
+python 04_gnn_prediction.py 2 12 bilbao False True
+python 04_gnn_prediction.py 2 24 bilbao False True
+python 04_gnn_prediction.py 2 48 bilbao False True
